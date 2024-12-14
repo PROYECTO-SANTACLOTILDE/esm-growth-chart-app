cd144e7e0ffaef880ed1da59e68952a772ba7a9a
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2024-12-14 12:29:32 -0500

    growth chart first try!

--- src/charts/extensions/growth-chart.component.tsx
+++ src/charts/extensions/growth-chart.component.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
-import styles from './chart.scss';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Tile } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
+import styles from './growthChart.module.scss';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const GrowthChart: React.FC = () => {
+  const { t } = useTranslation();
+
+  const data = {
+    labels: ['0', '1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: t('growthChart.height'),
+        data: [50, 55, 60, 65, 70, 75, 80],
+        borderColor: '#00A9E0',
+        tension: 0.1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: t('growthChart.title'),
+      },
+    },
+  };
+
   return (
-    <div className={styles.gray}>
-      Soy un growth chart
-      <p>Lorem Ipsum</p>
-    </div>
+    <Tile className={styles.growthChartTile}>
+      <h3>{t('growthChart.title')}</h3>
+      <div className={styles.chartContainer}>
+        <Line data={data} options={options} />
+      </div>
+    </Tile>
   );
 };
 
