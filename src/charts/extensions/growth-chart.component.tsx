fd20c9220ca9584164d40b018155b10d6439c102
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2024-12-23 13:27:59 -0500

    Add @carbon/charts-react dependency and update growth-chart.component.tsx

--- src/charts/extensions/growth-chart.component.tsx
+++ src/charts/extensions/growth-chart.component.tsx
@@ -1,53 +1,85 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { uniqBy } from 'lodash-es';
-import useSWR from 'swr';
+import { ComboChart, ScaleTypes } from '@carbon/charts-react';
+import '@carbon/charts/styles.css';
 import { Tile } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
-import { useSession, useConfig } from '@openmrs/esm-framework';
+import { useConfig } from '@openmrs/esm-framework';
 import { type Config } from '../../config-schema';
 import styles from './growthChart.module.scss';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
-
 const GrowthChart: React.FC = () => {
   const { t } = useTranslation();
+  const config: Config = useConfig();
 
-  const data = {
-    labels: ['0', '1', '2', '3', '4', '5', '6'],
-    datasets: [
-      {
-        label: t('growthChart.height'),
-        data: [50, 55, 60, 65, 70, 75, 80],
-        borderColor: '#00A9E0',
-        tension: 0.1,
-      },
-    ],
-  };
+  const data = [
+    {
+      group: t('growthChart.height'),
+      key: '0',
+      value: 50,
+    },
+    {
+      group: t('growthChart.height'),
+      key: '1',
+
+      value: 55,
+    },
+    {
+      group: t('growthChart.height'),
+      key: '2',
+      value: 60,
+    },
+    {
+      group: t('growthChart.height'),
+      key: '3',
+      value: 65,
+    },
+    {
+      group: t('growthChart.height'),
+      key: '4',
+      value: 70,
+    },
+    {
+      group: t('growthChart.height'),
+      key: '5',
+      value: 75,
+    },
+    {
+      group: t('growthChart.height'),
+      key: '6',
+      value: 80,
+    },
+  ];
 
   const options = {
-    responsive: true,
-    plugins: {
-      title: {
-        display: true,
-        text: t('growthChart.title'),
+    title: t('growthChart.title'),
+    axes: {
+      left: {
+        title: t('growthChart.axisHeight'),
+        mapsTo: 'value',
+      },
+      bottom: {
+        scaleType: ScaleTypes.LABELS,
+        mapsTo: 'key',
       },
     },
+    comboChartTypes: [
+      {
+        type: 'line',
+        options: {
+          points: {
+            enabled: true,
+          },
+        },
+        correspondingDatasets: [t('growthChart.height')],
+      },
+    ],
+    curve: 'curveNatural',
+    height: '400px',
   };
 
   return (
     <Tile className={styles.white}>
-      <Line data={data} options={options} />
+      <ComboChart data={data} options={options}></ComboChart>
     </Tile>
   );
 };
