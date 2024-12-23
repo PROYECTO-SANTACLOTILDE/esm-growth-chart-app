49db242e012092c22c09ec18064d29a87a88db77
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2024-12-23 12:36:52 -0500

    Remove unused box components from routes.json

--- src/obstetricia.component.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ConfigurableLink } from '@openmrs/esm-framework';
-import { useTranslation } from 'react-i18next';
-
-export default function DispensingLink() {
-  const { t } = useTranslation();
-  return <ConfigurableLink to={'https://www.instagram.com/'}>{t('dispensing', 'TEXTO VACIO')}</ConfigurableLink>;
-}
