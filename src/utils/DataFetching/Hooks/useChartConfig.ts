471cbfc558ce141342fbca5c04c40c6d63f30cec
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-01-27 22:41:44 -0500

    Agregar comentarios descriptivos en la configuración del gráfico y mejorar la función fetcher para mayor claridad

--- src/utils/DataFetching/Hooks/useChartConfig.ts
+++ src/utils/DataFetching/Hooks/useChartConfig.ts
@@ -2,31 +2,32 @@ import useSWR, { useSWRConfig } from 'swr';
 import { openmrsFetch } from '@openmrs/esm-framework'; // Función para realizar solicitudes en OpenMRS
 
 export type ChartConfig = {
+  //estructura de datos de la configuración del gráfico
     metadata: {
         attributes: {
-            dateOfBirth: string;
-            gender: string;
-            femaleOptionCode: string;
-            maleOptionCode: string;
+            dateOfBirth: string; //UUIDs del concepto detras de fecha de nacimiento
+            gender: string; //UUIDs del concepto detras de genero
+            femaleOptionCode: string; //UUIDs de MUJER
+            maleOptionCode: string; //UUIDs de HOMBRE
         };
         dataElements: {
-            headCircumference: string;
-            height: string;
-            weight: string;
+            headCircumference: string; //UUIDs del concepto
+            height: string; //UUIDs del concepto
+            weight: string; //UUIDs del concepto
         };
         program: {
-            programStageId: string;
+            programStageId: string; //UUIDs del programa
         };
     };
     settings: {
-        customReferences: boolean;
-        usePercentiles: boolean;
-        weightInGrams: boolean;
-        defaultIndicator: string;
+        customReferences: boolean; //settings que extraigo del mismo front (con el hook config de openmrs-framework o de la BD desde la tabla systemsetting)
+        usePercentiles: boolean; //settings que extraigo del mismo front (con el hook config de openmrs-framework o de la BD desde la tabla systemsetting)
+        weightInGrams: boolean; //settings que extraigo del mismo front (con el hook config de openmrs-framework o de la BD desde la tabla systemsetting)
+        defaultIndicator: string; //settings que extraigo del mismo front (con el hook config de openmrs-framework o de la BD desde la tabla systemsetting)
     };
 };
 
-// Función fetcher para SWR que usa la API de OpenMRS
+// Función fetcher para SWR que usa la API de OpenMRS, la otra opcion es usar configSchema y useConfig de '@openmrs/esm-framework' para almacenar esas variables;
 const fetchChartConfig = async (): Promise<ChartConfig> => {
     const response = await openmrsFetch('/ws/rest/v1/systemsetting/capture-growth-chart.config'); // Ruta API
     if (!response.ok) {
@@ -35,6 +36,7 @@ const fetchChartConfig = async (): Promise<ChartConfig> => {
     return response.data;
 };
 
+//aca deberia cargar la estructura de datos de la configuración del gráfico con el fetch de setting y los UUIDs.
 export const useChartConfig = () => {
     const { data, error, isValidating } = useSWR<ChartConfig>('chartConfig', fetchChartConfig, {
         revalidateOnFocus: true, // Opcional: Actualiza al volver a enfocar la ventana
