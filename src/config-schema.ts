433db788713fbb76524ee48321d575d02ba8c8db
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-01-29 01:18:32 -0500

    Agregar nuevos conceptos y configuraciones para signos vitales y biometría en el esquema de configuración

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -74,6 +74,106 @@ export const configSchema = {
       _default: 'wfa', // "Weight for age"
     },
   },
+  concepts: {
+    systolicBloodPressureUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5085AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    diastolicBloodPressureUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    pulseUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5087AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    temperatureUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5088AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    oxygenSaturationUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5092AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    heightUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5090AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    weightUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5089AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    respiratoryRateUuid: {
+      _type: Type.ConceptUuid,
+      _default: '5242AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    generalPatientNoteUuid: {
+      _type: Type.ConceptUuid,
+      _default: '165095AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    midUpperArmCircumferenceUuid: {
+      _type: Type.ConceptUuid,
+      _default: '1343AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    vitalSignsConceptSetUuid: {
+      _type: Type.ConceptUuid,
+      _default: '1114AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+  },
+  vitals: {
+    useFormEngine: {
+      _type: Type.Boolean,
+      _default: false,
+      _description:
+        'Whether to use an Ampath form as the vitals and biometrics form. If set to true, encounterUuid and formUuid must be set as well.',
+    },
+    encounterTypeUuid: {
+      _type: Type.UUID,
+      _default: '67a71486-1a54-468f-ac3e-7091a9a79584',
+    },
+    logo: {
+      src: {
+        _type: Type.String,
+        _default: null,
+        _description: 'A path or URL to an image. Defaults to the OpenMRS SVG sprite.',
+      },
+      alt: {
+        _type: Type.String,
+        _default: 'Logo',
+        _description: 'Alt text, shown on hover',
+      },
+      name: {
+        _type: Type.String,
+        _default: null,
+        _description: 'The organization name displayed when image is absent',
+      },
+    },
+    showPrintButton: {
+      _type: Type.Boolean,
+      _default: false,
+      _description:
+        'Determines whether or not to display the Print button in the vitals datatable header. If set to true, a Print button gets shown as the right-most item in the table header. When clicked, this button enables the user to print out the contents of the table',
+    },
+    formUuid: {
+      _type: Type.UUID,
+      _default: '9f26aad4-244a-46ca-be49-1196df1a8c9a',
+    },
+    formName: {
+      _type: Type.String,
+      _default: 'Vitals',
+    },
+    useMuacColors: {
+      _type: Type.Boolean,
+      _default: false,
+      _description: 'Whether to show/use MUAC color codes. If set to true, the input will show status colors.',
+    },
+  },
+  biometrics: {
+    bmiUnit: {
+      _type: Type.String,
+      _default: 'kg / m²',
+    },
+  },
 };
 
 export type ChartConfig = {
