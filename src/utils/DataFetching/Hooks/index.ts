f141f5365700cb99c504c32ecc344215ae10e9b9
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-02-04 14:33:20 -0500

    Agregar el hook usePatientObservations para mejorar la gestión de datos del paciente.

--- src/utils/DataFetching/Hooks/index.ts
+++ src/utils/DataFetching/Hooks/index.ts
@@ -3,3 +3,4 @@ export { handleAPIResponse } from './handleAPIResponse';
 export { useVitalsAndBiometrics } from './useVitals';
 export { useChartConfig } from './useChartConfig';
 export { useCustomReferences } from './useCustomReferences';
+export { usePatientObservations } from './usePatientObservations';
