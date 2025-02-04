b2b22b8d34484129ef988f193ce8082e5b4069f7
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-02-04 14:47:01 -0500

    Eliminar la importación de useTranslation en el componente Root para simplificar el código.

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import GrowthChartOverview from './charts/extensions/GrowthChart/growthchart-overview';
 import { useConfig } from '@openmrs/esm-framework';
 
 const Root: React.FC = () => {
-  const { t } = useTranslation();
   const config = useConfig();
 
   const patientUuid = '5f088298-da75-420a-8413-84dddefc8f66';
